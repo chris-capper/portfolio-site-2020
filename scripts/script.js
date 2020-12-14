527192d4fb2d168d304c3e5c64067056ce059339
Chris Capper <55027395+chris-capper@users.noreply.github.com>
2020-12-13 19:21:40 -0800

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+let btn1 = document.getElementById("btn-1");
+let btn2 = document.getElementById("btn-2");
+let btn3 = document.getElementById("btn-3");
+let btn4 = document.getElementById("btn-4");
+
+function openWelcome() {
+    btn1.style.color = "#000000";
+    btn1.style.backgroundColor = "#b4a9be";
+    btn1.style.boxShadow = "inset 0 4px 4px 1px rgba(0, 0, 0, 0.14), inset 0 6px 2px -1px rgba(0, 0, 0, 0.2), inset 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn1.style.transition = "0.1s";
+    btn2.style.color = "#f6f3f2";
+    btn2.style.backgroundColor = "#444887";
+    btn2.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn3.style.color = "#f6f3f2";
+    btn3.style.backgroundColor = "#444887";
+    btn3.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn4.style.color = "#f6f3f2";
+    btn4.style.backgroundColor = "#444887";
+    btn4.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+}
+
+function openServices() {
+    btn1.style.color = "#f6f3f2";
+    btn1.style.backgroundColor = "#444887";
+    btn1.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn2.style.color = "#000000";
+    btn2.style.backgroundColor = "#b4a9be";
+    btn2.style.boxShadow = "inset 0 4px 4px 1px rgba(0, 0, 0, 0.14), inset 0 6px 2px -1px rgba(0, 0, 0, 0.2), inset 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn2.style.transition = "0.1s";
+    btn3.style.color = "#f6f3f2";
+    btn3.style.backgroundColor = "#444887";
+    btn3.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn4.style.color = "#f6f3f2";
+    btn4.style.backgroundColor = "#444887";
+    btn4.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+
+}
+
+function openWork() {
+    btn1.style.color = "#f6f3f2";
+    btn1.style.backgroundColor = "#444887";
+    btn1.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn2.style.color = "#f6f3f2";
+    btn2.style.backgroundColor = "#444887";
+    btn2.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn3.style.color = "#000000";
+    btn3.style.backgroundColor = "#b4a9be";
+    btn3.style.boxShadow = "inset 0 4px 4px 1px rgba(0, 0, 0, 0.14), inset 0 6px 2px -1px rgba(0, 0, 0, 0.2), inset 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn3.style.transition = "0.1s";
+    btn4.style.color = "#f6f3f2";
+    btn4.style.backgroundColor = "#444887";
+    btn4.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+}
+
+function openContact() {
+    btn1.style.color = "#f6f3f2";
+    btn1.style.backgroundColor = "#444887";
+    btn1.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn2.style.color = "#f6f3f2";
+    btn2.style.backgroundColor = "#444887";
+    btn2.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn3.style.color = "#f6f3f2";
+    btn3.style.backgroundColor = "#444887";
+    btn3.style.boxShadow = "0 4px 4px 1px rgba(0, 0, 0, 0.14), 0 6px 2px -1px rgba(0, 0, 0, 0.2), 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn4.style.color = "#000000";
+    btn4.style.backgroundColor = "#b4a9be";
+    btn4.style.boxShadow = "inset 0 4px 4px 1px rgba(0, 0, 0, 0.14), inset 0 6px 2px -1px rgba(0, 0, 0, 0.2), inset 0 2px 10px 1px rgba(0, 0, 0, 0.12)";
+    btn4.style.transition = "0.1s";
+}
+
+/* fading img gallery on front page */
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first').fadeOut(1000).next().fadeIn(1000).end().appendTo('#slideshow');},  3000);
+
+/* navigation to top of page */
+$("#btn-1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#welcome").offset().top-275
+    }, 500);
+});
+
+/* navigation to services section */
+$("#btn-2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top-160
+    }, 500);
+});
+
+/* navigation to work section */
+$("#btn-3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top-160
+    }, 500);
+});
+
+/* navigation to contact form */
+$("#btn-4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 500);
+});
